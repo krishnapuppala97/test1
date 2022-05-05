49d64284ae651ed8c895ac0aad022a1c7876d90b
mani5a3 <sivakrishna5a35a3@gmail.com>
2022-05-05 23:40:57 +0530

    syntax issue

--- environments/dev/module.lb.tf
+++ environments/dev/module.lb.tf
@@ -1,6 +1,7 @@
-module "test-alb" {
+module "prod-alb" {
   source = "../../modules/alb"
-  load_balancer_name = "test-alb"
+  load_balancer_name = "ConceptOne-PROD-Lb"
   load_balancer_type = "application"
   security_groups    =  [module.test-sg.sg_id]
+  subnets = ["subnet-0c2b8303bcfcc73dd","	subnet-0b01a8beb2ae71bd7","subnet-04e39614de0b6e253"]
 }
