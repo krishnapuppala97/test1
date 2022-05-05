12f12b76532676991bc4a5ab6c08d60f7518e180
mani5a3 <sivakrishna5a35a3@gmail.com>
2022-05-05 23:57:43 +0530

    syntax issue

--- modules/alb/load-balancer.tf
+++ modules/alb/load-balancer.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "lb" {
   internal                   = var.internal
   load_balancer_type         = var.load_balancer_type
   security_groups            = var.security_groups
+  subnets                    = var.subnets
 /*   lifecycle {
     create_before_destroy = true
   } */
