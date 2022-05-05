ec821b90d50b1aaf5660529341298d1704065ac5
mani5a3 <sivakrishna5a35a3@gmail.com>
2022-05-05 23:23:17 +0530

    terraform tasks

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,8 +3,7 @@ resource "aws_instance" "instance" {
   instance_type        = var.instance_type
   user_data            = var.user_data
   user_data_base64     = var.user_data_base64
-  availability_zone      = var.availability_zone
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = var.vpc_security_x`group_ids
   key_name             = var.key_name
   iam_instance_profile = var.iam_instance_profile
   associate_public_ip_address = var.associate_public_ip_address
